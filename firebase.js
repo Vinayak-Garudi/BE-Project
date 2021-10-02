52f1ed20671bbdd5554b7b5dcec1c3074307839f
Vinayak Garudi <88715077+Vinayak-Garudi@users.noreply.github.com>
2021-10-02 16:21:10 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+              apiKey: "AIzaSyCoO9YcKZJdEpkMsfa51XBqbJz6eMAVW8A",
+              authDomain: "be-project-94a2e.firebaseapp.com",
+              projectId: "be-project-94a2e",
+              storageBucket: "be-project-94a2e.appspot.com",
+              messagingSenderId: "504096098693",
+              appId: "1:504096098693:web:32f6444cbdaf25284e1cec",
+              measurementId: "G-CKMJ10QJD5"
+            };
+          
+            // Initialize Firebase
+            const app = initializeApp(firebaseConfig);
+            const analytics = getAnalytics(app);
